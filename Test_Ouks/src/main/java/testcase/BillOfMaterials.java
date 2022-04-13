8e25013e966404ed4969fd4384915d9db767d5b7
ouks-bd <obyed.oss@gmail.com>
2022-04-13 15:32:55 +0600

    bom

--- Test_Ouks/src/main/java/testcase/BillOfMaterials.java
+++ Test_Ouks/src/main/java/testcase/BillOfMaterials.java
@@ -2,7 +2,10 @@ package testcase;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+
+import pom.Page_Object_Model;
 
 public class BillOfMaterials extends driver {
 	
@@ -15,7 +18,10 @@ String baseUrl="http://test.beacontech.xyz/";
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
 
-		
+		Page_Object_Model user=new Page_Object_Model();
+		driver.findElement(By.id("UserName")).sendKeys(user.username);
+		driver.findElement(By.id("Password")).sendKeys(user.password);
+		driver.findElement(By.className("col-xs-4")).click();
 		
 }
 }
