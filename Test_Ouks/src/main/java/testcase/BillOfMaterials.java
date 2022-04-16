35feb1437307a1b9ecc69759af54281d226090f3
ouks-bd <obyed.oss@gmail.com>
2022-04-16 15:13:02 +0600

    bom_fabric_Add

--- Test_Ouks/src/main/java/testcase/BillOfMaterials.java
+++ Test_Ouks/src/main/java/testcase/BillOfMaterials.java
@@ -3,6 +3,8 @@ package testcase;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import pom.Page_Object_Model;
@@ -24,5 +26,44 @@ String baseUrl="http://test.beacontech.xyz/";
 		driver.findElement(By.className("col-xs-4")).click();
 		
 		driver.get("http://test.beacontech.xyz/Merchandising/OrderView/608");
+		
+		WebElement ItemFK_2 = driver.findElement(By.id("ItemFK_2"));
+		Select BOM_Fabric  =new Select(ItemFK_2);
+		BOM_Fabric.selectByValue("6904");
+		BOM_Fabric.selectByValue("6905");
+		Thread.sleep(2000);
+		
+		//selectByIndex(1);
+		
+		//BOM_Fabric_color_Common_ColorFK
+		WebElement Fabric_colour = driver.findElement(By.id("Common_ColorFK"));
+		Select BOM_Fabric_c  =new Select(Fabric_colour);
+		BOM_Fabric_c.selectByValue("1");
+		
+		
+		//driver.findElement(By.id("vmBOM.GSM")).clear();
+		driver.findElement(By.xpath("//input[@name='vmBOM.GSM']")).sendKeys("160");
+		Thread.sleep(2000);
+		
+		//driver.findElement(By.id("txt5")).sendKeys("500");
+		
+		//driver.findElement(By.id("vmBOM.Consumption")).clear();
+		WebElement consumption=driver.findElement(By.name("vmBOM.Consumption"));
+        consumption.sendKeys("500");
+        
+        //driver.findElement(By.id("vmBOM.ConRequiredQty")).clear();
+        WebElement required_quantity=driver.findElement(By.name("vmBOM.ConRequiredQty"));
+        required_quantity.sendKeys("5");
+        
+		WebElement tolerence=driver.findElement(By.name("vmBOM.Tolerance"));
+        tolerence.sendKeys("0.1");
+     
+        //driver.findElement(By.id("vmBOM.UnitPrice")).clear();
+        WebElement UnitPrice=driver.findElement(By.name("vmBOM.UnitPrice"));
+        UnitPrice.sendKeys("10");
+        Thread.sleep(2000);
+        
+        driver.findElement(By.id("btnsave")).click();
+        
 }
 }
