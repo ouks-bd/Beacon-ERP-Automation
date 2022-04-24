f075f2d0a792dd952936ec51f14115b4b4591bfb
ouks-bd <obyed.oss@gmail.com>
2022-04-24 12:10:58 +0600

    checkbox

--- Test_Ouks/src/main/java/testcase/PurchaseRequisiotion.java
+++ Test_Ouks/src/main/java/testcase/PurchaseRequisiotion.java
@@ -28,21 +28,42 @@ public class PurchaseRequisiotion extends driver {
 	
 	driver.get("http://test.beacontech.xyz/procurement/procurementstylepurchaserequisitionslave");
 	
-	driver.findElement(By.id("//input[@id='txtRequisitionDate']")).sendKeys("2022-04-03");
+	driver.findElement(By.id("txtRequisitionDate")).sendKeys("2022-04-03");
+	Thread.sleep(5000);
 	
-	WebElement PrType = driver.findElement(By.id("ddlProcurementOriginTypeEnumFK"));
-	Select PrTypeBom  =new Select(PrType);
+	WebElement PrOrigin = driver.findElement(By.id("ddlProcurementOriginTypeEnumFK"));
+	Select PrTypeBom  =new Select(PrOrigin);
 	PrTypeBom.selectByValue("2");
-	
-	WebElement PrType = driver.findElement(By.id("ddlMerchandisingStyleID"));
-	Select PrTypeBom  =new Select(PrType);
-	PrTypeBom.selectByValue("2");
+	Thread.sleep(5000);
 	
 	
+	//WebElement PrType = driver.findElement(By.xpath("//*[@class=\"form-control ddlReset"]")); 
+	//WebElement Pr = driver.findElement(By.className("form-control ddlReset"));
+	WebElement Pr = driver.findElement(By.name("RequisitionTypeEnumFK"));
+	Select PrTypeProcurement  =new Select(Pr);
+	PrTypeProcurement.selectByValue("1");
+	Thread.sleep(5000);
 	
-	KIK/00015A/20/08/17/P121330/Baby Boy Knitted Set, Shirt+Trouser
+	
+	WebElement PrStyle = driver.findElement(By.id("ddlMerchandisingStyleID"));
+	Select PrStyleBom  =new Select(PrStyle);
+	PrStyleBom.selectByVisibleText("KIK/00015A/20/08/17/P121330/Baby Boy Knitted Set, Shirt+Trouser");
+	Thread.sleep(5000);
+	
+//	WebElement PrStyleSelect = driver.findElement(By.id("txtRequisitionQuantity_0"));
+//	Select PrStyleSelectBom  =new Select(PrStyleSelect);
+//	PrStyleSelectBom.selectByValue("0.00450");
+	
+	driver.findElement(By.id("select_all")).click();
+//	driver.findElement(By.id("txtRequisitionQuantity_0"));
+//	driver.findElement(By.id("DataListPartial_0__Flag"));
+//	driver.findElement(By.id("DataListPartial_1__Flag"));
 	
 	
+	
+	Thread.sleep(5000);
+	
+		
 }
 }
 
