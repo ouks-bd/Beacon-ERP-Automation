dcbd3d79e115d4dc8a30986fe292b8c5e0c0f5f6
ouks-bd <obyed.oss@gmail.com>
2022-04-29 19:55:16 +0600

    poreceivingloging

--- Test_Ouks/src/main/java/testcase/PoReceving.java
+++ Test_Ouks/src/main/java/testcase/PoReceving.java
@@ -1,7 +1,31 @@
 package testcase;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import pom.Page_Object_Model;
+
 public class PoReceving  extends driver{
 	
+String baseUrl="http://test.beacontech.xyz/";
+	
+	
+	@Test
+	public void locatorlearning() throws InterruptedException {
+
+	driver.get(baseUrl);
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	driver.manage().window().maximize();
+
+
+	Page_Object_Model user=new Page_Object_Model();
+	driver.findElement(By.id("UserName")).sendKeys(user.username);
+	driver.findElement(By.id("Password")).sendKeys(user.password);
+	driver.findElement(By.className("col-xs-4")).click();
 	
 
 }
+}
+
