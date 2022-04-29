40dc6ac2ca932e1b17641f164e1cfdb3c4b0006d
ouks-bd <obyed.oss@gmail.com>
2022-04-29 19:57:18 +0600

    poreceivingget

--- Test_Ouks/src/main/java/testcase/PoReceving.java
+++ Test_Ouks/src/main/java/testcase/PoReceving.java
@@ -25,7 +25,9 @@ String baseUrl="http://test.beacontech.xyz/";
 	driver.findElement(By.id("Password")).sendKeys(user.password);
 	driver.findElement(By.className("col-xs-4")).click();
 	
+	driver.get("http://test.beacontech.xyz/warehouse/warehouseporeceivingslave");
 
+	
 }
 }
 
