9b3aaf62059e5cf8d6e5e721eb987c76b7fc8ef4
ouks-bd <obyed.oss@gmail.com>
2022-05-10 17:28:08 +0600

    login

--- Test_Ouks/src/main/java/testcase/ShipmentSchedule.java
+++ Test_Ouks/src/main/java/testcase/ShipmentSchedule.java
@@ -1,7 +1,41 @@
 package testcase;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import pom.Page_Object_Model;
+
 public class ShipmentSchedule extends driver{
 	
-	
+	@Test
+	public void test () throws InterruptedException {
+		
+		driver.get(baseUrl);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		
+		//get_title
+		String actualTitleOfTheSite=driver.getTitle();
+		//String titleOfThePage=driver.getTitle();
+		System.out.println("The Title of the site is: "+ actualTitleOfTheSite);
+		//Assert.assertEquals("My Stores", actualTitleOfTheSite);
+		
+		
+		
+		Thread.sleep(3000);
+		Page_Object_Model user = new Page_Object_Model();
+		
+		//log_IN
+		driver.findElement(By.id("UserName")).sendKeys(user.username);
+		
+		//driver.findElement(By.id("Password")).sendKeys(user.password);
+		
+		WebElement Password = driver.findElement(By.id("Password"));
+		Password.click();
+		Password.sendKeys("floydshoeb$");
 
+}
 }
