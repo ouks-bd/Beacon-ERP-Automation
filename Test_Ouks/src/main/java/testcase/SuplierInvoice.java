7c59a1df9ed4db50792ef622184d162e054168a6
ouks-bd <obyed.oss@gmail.com>
2022-05-11 17:28:59 +0600

    invoice

--- Test_Ouks/src/main/java/testcase/SuplierInvoice.java
+++ Test_Ouks/src/main/java/testcase/SuplierInvoice.java
@@ -1,7 +1,37 @@
 package testcase;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import pom.Page_Object_Model;
+
 public class SuplierInvoice extends driver {
 
+	String baseUrl="http://test.beacontech.xyz/";	
+	@Test
+	public void test () throws InterruptedException {
+		
+		driver.get(baseUrl);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
 	
+		
+		Page_Object_Model user = new Page_Object_Model();
+		
+		//log_IN
+		driver.findElement(By.id("UserName")).sendKeys(user.username);
+		
+		driver.findElement(By.id("Password")).sendKeys(user.password);
+		
+		driver.findElement(By.className("col-xs-4")).click();
+		
+		driver.get("http://test.beacontech.xyz/Merchandising/StyleShipment/796");
+				Thread.sleep(5000);
+		
+}
+}
+
 	
 }
