a7bebc8cb70987564390cf335c2a180588dcc3b2
ouks-bd <obyed.oss@gmail.com>
2022-05-22 11:21:54 +0600

    createinvoice

--- Test_Ouks/src/main/java/testcase/SuplierInvoice.java
+++ Test_Ouks/src/main/java/testcase/SuplierInvoice.java
@@ -38,22 +38,33 @@ public class SuplierInvoice extends driver {
 		WebElement paymentmethod = driver.findElement(By.id("ddlSupplierPaymentMethodEnumFK"));
 		Select payment  =new Select(paymentmethod);
 		payment.selectByValue("1");
-		
+		Thread.sleep(5000);
 		
 		WebElement currency  = driver.findElement(By.id("ddlCurrencyFK"));
 		Select currencytype  =new Select(currency);
 		currencytype.selectByValue("2");
+		Thread.sleep(5000);
 		
 		WebElement supplierselect  = driver.findElement(By.id("ddlCommon_SupplierFK"));
 		Select suppliername    =new Select(supplierselect);
 		suppliername.selectByValue("1");
+		Thread.sleep(5000);
 		
 		WebElement ponumber  = driver.findElement(By.id("ddlProcurementPurchaseOrderFK"));
 		Select selectnumber    =new Select(ponumber);
 		selectnumber.selectByValue("3117");
+		Thread.sleep(5000);
 		
-		
+		driver.findElement(By.id("DataListPartial_0__InvoiceQuantity")).clear();
 		driver.findElement(By.id("DataListPartial_0__InvoiceQuantity")).sendKeys("1");
+		
+		
+		driver.findElement(By.id("DataListPartial_0__Flag")).click();
+		
+		driver.findElement(By.id("btnsave")).click();
+		
+		
+		
 		
 		Thread.sleep(5000);
 		
